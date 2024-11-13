c6bac662f60d447258c3ee2d4362e52303bc092a
Demetrius Papas <25155822+DemetriusPapas@users.noreply.github.com>
2024-11-13 23:09:28 +0200

    WIP: implementing jwt generation

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/DemetriusPapas/guarddoggo
 
 go 1.23.1
+
+require github.com/golang-jwt/jwt/v5 v5.2.1
--- jwt.go
+++ jwt.go
@@ -1,6 +1,11 @@
 package guarddoggo
 
-import "time"
+import (
+	"errors"
+	"time"
+
+	gjwt "github.com/golang-jwt/jwt/v5"
+)
 
 type jwt struct {
 	secret   string
@@ -13,4 +18,25 @@ func (s *jwt) validate() error {
 	// TODO: implement
 
 	return nil
+}
+
+// Command the doggo to fetch you a brand new jwt
+func (s *jwt) Fetch(userID string) (string, error) {
+	claims := gjwt.RegisteredClaims{
+		Issuer:    s.issuer,
+		IssuedAt:  gjwt.NewNumericDate(time.Now().UTC()),
+		ExpiresAt: gjwt.NewNumericDate(time.Now().UTC().Add(s.lifetime)),
+		Subject:   userID,
+	}
+
+	token := gjwt.NewWithClaims(gjwt.SigningMethodHS256, claims)
+
+	// TODO: Asymmetric option
+
+	signedToken, err := token.SignedString([]byte(s.secret))
+	if err != nil {
+		return "", errors.New("failed to sign token: " + err.Error())
+	}
+
+	return signedToken, nil
 }
